c49eaf932a0ed69dd084e1e8a01beaf90f5389ef
mani333007 <mani333007@gmail.com>
2021-06-16 16:06:57 +0530

    createSlice

--- store/todos.js
+++ store/todos.js
@@ -1,31 +1,33 @@
 //import { createAction } from '@reduxjs/toolkit'
 import pkg from '@reduxjs/toolkit';
-const { createAction, createReducer } = pkg;
+const { createSlice } = pkg;
 
-export const todoAdded = createAction('todoAdded');
-export const todoRemoved = createAction('todoRemoved');
-export const todoToggleCompleate = createAction('todoToggleCompleate');
-
-// [ { id: 1, description: 'Do yoga', compleate: false } ]
 let id = 0;
 
-const todosReducer = createReducer([], (builder) => {
-    builder
-        .addCase(todoAdded, (todos, action) => {
-            todos.push({ 
+const slice = createSlice({
+    name: 'todos',
+    initialState: [],
+    reducers: {
+        todoAdded: (todos, action) => {
+            console.log(todos);
+            todos.push({
                 _id: ++id, 
                 description: action.payload.description, 
-                compleate: false 
+                compleate: false
             })
-        })
-        .addCase(todoRemoved, (todos, action) => {
+        },
+        todoRemoved: (todos, action) => {
             const index = todos.findIndex(todo => todo._id === action.payload._id);
             todos.splice(index, 1);
-        })
-        .addCase(todoToggleCompleate, (todos, action) => {
+        },
+        todoToggleCompleate: (todos, action) => {
             const index = todos.findIndex(todo => todo._id === action.payload._id);
             todos[index].compleate = !todos[index].compleate;
-        })
-})
+        }
+    }
+});
 
-export default todosReducer
+export const { todoAdded, todoRemoved, todoToggleCompleate } = slice.actions;
+export default slice.reducer
+
+

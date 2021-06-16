936e591f283d3d49e83d28efc7d5106984581952
mani333007 <mani333007@gmail.com>
2021-06-16 15:17:29 +0530

    Builder Callback Notation

--- store/todos.js
+++ store/todos.js
@@ -8,22 +8,24 @@ export const todoToggleCompleate = createAction('todoToggleCompleate');
 
 // [ { id: 1, description: 'Do yoga', compleate: false } ]
 let id = 0;
-const todosReducer = createReducer([], {
-    todoAdded: (todos, action) => {
-        todos.push({ 
-            _id: ++id, 
-            description: action.payload.description, 
-            compleate: false 
+
+const todosReducer = createReducer([], (builder) => {
+    builder
+        .addCase(todoAdded, (todos, action) => {
+            todos.push({ 
+                _id: ++id, 
+                description: action.payload.description, 
+                compleate: false 
+            })
         })
-    },
-    todoRemoved: (todos, action) => {
-        const index = todos.findIndex(todo => todo._id === action.payload._id);
-        todos.splice(index, 1);
-    },
-    todoToggleCompleate: (todos, action) => {
-        const index = todos.findIndex(todo => todo._id === action.payload._id);
-        todos[index].compleate = !todos[index].compleate
-    }
-});
+        .addCase(todoRemoved, (todos, action) => {
+            const index = todos.findIndex(todo => todo._id === action.payload._id);
+            todos.splice(index, 1);
+        })
+        .addCase(todoToggleCompleate, (todos, action) => {
+            const index = todos.findIndex(todo => todo._id === action.payload._id);
+            todos[index].compleate = !todos[index].compleate;
+        })
+})
 
 export default todosReducer
